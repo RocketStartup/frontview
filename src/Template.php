4555b184d0ece899fa610287a78d5d46daf5e48f
ClebersonBieleski <clebersonbieleski@gmail.com>
2019-08-14 23:24:11 -0300

    Add compress

--- src/Template.php
+++ src/Template.php
@@ -8,6 +8,7 @@ class Template{
 	private $replaceAllHtml     	= array();
 	
 	private $useCache 				= false;
+	private $compress 				= false;
 	private $twig 			 		= null;
 	private $fileTemplate 			= '404.tpl';
 	private $pathTemplate 			= 'templates/';
@@ -18,47 +19,40 @@ class Template{
 		return $this;
 	}
 
-
-	public function render(){
+	public function generateHtml(){
 		if(!empty($this->fileTemplate()) && file_exists($this->getPathTemplate().$this->fileTemplate())){
 			$timer=\Performace::getInstance('Timer');
 			$timer->register('twig',microtime(true));
-		
 			$loader = new \Twig\Loader\FilesystemLoader($this->getPathTemplate());
-
 			$this->twig = new \Twig\Environment($loader, [
 				'cache' => ($this->useCache()==false?false:PATH_ROOT.$this->pathCache.$this->nameTemplate())
 			]);
-
-			echo $this->twig->render($this->fileTemplate(), $this->replaceAllHtml);
-			$timer->register('twig',microtime(true));
-		}else{
-			throw new \Exception('Template '.$this->fileTemplate().' not found '.$this->getPathTemplate().$this->fileTemplate(), 1);
 			
-		}
-	}
-
-	public function returnHtml(){
-		if(!empty($this->fileTemplate()) && file_exists($this->getPathTemplate().$this->fileTemplate())){
-			$timer=\Performace::getInstance('Timer');
+			if($this->compress()==true){
+				$search = array(
+					'/\>[^\S ]+/s',  // remove whitespaces after tags
+					'/[^\S ]+\</s',  // remove whitespaces before tags
+					'/(\s)+/s'       // remove multiple whitespace sequences
+				);
+				$replace = array('>','<','\\1');
+				$ret = preg_replace($search, $replace, $this->twig->render($this->fileTemplate(), $this->replaceAllHtml));
+			}else{
+				$ret = $this->twig->render($this->fileTemplate(), $this->replaceAllHtml);
+			}
 			$timer->register('twig',microtime(true));
-		
-			$loader = new \Twig\Loader\FilesystemLoader($this->getPathTemplate());
-
-			$this->twig = new \Twig\Environment($loader, [
-				'cache' => ($this->useCache()==false?false:PATH_ROOT.$this->pathCache.$this->nameTemplate())
-			]);
-
-			$html =  $this->twig->render($this->fileTemplate(), $this->replaceAllHtml);
-			$timer->register('twig',microtime(true));
-
-			return $html;
+			return $ret;
 		}else{
 			throw new \Exception('Template '.$this->fileTemplate().' not found '.$this->getPathTemplate().$this->fileTemplate(), 1);
 			
 		}
 	}
 	
+	public function render(){
+		echo $this->generateHtml();
+	}
+	public function returnHtml(){
+		return $this->generateHtml();
+	}
 	
 	public function nameTemplate($v=null){
 		if($v==null){
@@ -89,7 +83,6 @@ class Template{
 		return PATH_ROOT.$this->pathTemplate.$this->nameTemplate().'/src/views/';
 	}
 
-	
 	public function useCache($v=null){
 		if(is_bool($v)){
 			$this->useCache=$v;
@@ -97,7 +90,14 @@ class Template{
 		}else{
 			return $this->useCache;
 		}
-		
 	}
-
+	
+	public function compress($v=null){
+		if(is_bool($v)){
+			$this->compress=$v;
+			return $this;
+		}else{
+			return $this->compress;
+		}
+	}
 }
